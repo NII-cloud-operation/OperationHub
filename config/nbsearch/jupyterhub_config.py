c4bc60ca24a445426f47e851bad1757fb8912476
Satoshi Yazawa <968739+yacchin1205@users.noreply.github.com>
2023-05-18 12:21:09 +0000

    Fix directories

--- config/nbsearch/jupyterhub_config.py
+++ config/nbsearch/jupyterhub_config.py
@@ -1,9 +1,9 @@
-service_name = 'solr'
+service_name  = 'solr'
 
 c.JupyterHub.services.append(
     {
         'name': service_name,
         'admin': True,
-        'url': "http://nbsearch-solr-proxy",
+        'url': f'http://nbsearch-{service_name}-proxy',
     }
 )

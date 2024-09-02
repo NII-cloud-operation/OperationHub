262ec5e541bc95d5916f8385bc3b126a9f70878d
Satoshi Yazawa <968739+yacchin1205@users.noreply.github.com>
2024-09-02 12:52:17 +0000

    Apply OpenID Connect authentication to nbsearch

--- config/oidc/jupyterhub_config.py
+++ config/oidc/jupyterhub_config.py
@@ -21,13 +21,24 @@ if enable_ep_weave is not None and bool(strtobool(enable_ep_weave)):
         "redirect_uris": [f"https://{server_name}/services/ep_weave/ep_openid_connect/callback"],
     })
 
+enable_nbsearch = os.environ.get('NBSEARCHDB_ENABLE_OIDC_SERVICE', None)
+if enable_nbsearch is not None and bool(strtobool(enable_nbsearch)):
+    oidc_services.append({
+        "oauth_client_id": os.environ['NBSEARCHDB_OAUTH_CLIENT_ID'],
+        "api_token": os.environ['NBSEARCHDB_OAUTH_CLIENT_SECRET'],
+        "redirect_uris": [f"https://{server_name}/services/solr/oauth2/callback"],
+    })
+
 if len(oidc_services) > 0:
     configure_jupyterhub_oidcp(
         c,
+        issuer="http://jupyterhub:8000/services/oidcp/internal/",
         base_url=f"https://{server_name}/",
         internal_base_url="http://jupyterhub:8000",
         debug=True,
         services=oidc_services,
         oauth_client_allowed_scopes=None,
         vault_path="./tmp/jupyterhub_oid/.vault",
+        admin_email_pattern="{uid}@admin.jupyterhub",
+        user_email_pattern="{uid}@user.jupyterhub",
     )
